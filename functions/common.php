007f0212246e27ee4bda9a3c57f8f5d366c5dbc2
Martin Kokeš <info@martinkokes.cz>
2017-08-05 14:45:11 +0200

    added PHP

--- functions/common.php
+++ functions/common.php
@@ -1,12 +1,13 @@
+<?php
 function printPictureSVG($name, $alt, $classes = '')
 {
-?>
+    ?>
   <picture class="<?php echo $classes; ?>">
     <source
     type="image/svg+xml"
     srcset="<?php echo get_template_directory_uri() . '/dist/images/' . $name . '.svg'; ?>">
     <img
-    src="<?php echo get_template_directory_uri() . '/dist/images/' . $name . '.png'; ?>" 
+    src="<?php echo get_template_directory_uri() . '/dist/images/' . $name . '.png'; ?>"
     alt="<?php echo $alt; ?>">
   </picture>
 <?php
